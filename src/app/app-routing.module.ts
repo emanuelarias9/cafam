55e405d5fc6ba06383298e244b13f77a4f6c89c1
Emanuel arias <Emanuel.arias@agtigroup.com>
2020-09-28 21:38:52 -0500

    Pagina categorias generada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'categorias',
+    loadChildren: () => import('./categorias/categorias.module').then( m => m.CategoriasPageModule)
+  },
 ];
 
 @NgModule({
